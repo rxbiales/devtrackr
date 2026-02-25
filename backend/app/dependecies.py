7ce67798138ef4960bdf0c797127d54dbf464b23
rene rubiales <rxbiales>
2026-02-25 16:36:01 -0300

    infra: update .gitignore to ignore python cache files

--- backend/app/dependecies.py
+++
@@ -1,9 +0,0 @@
-from .database import SessionLocal
-
-# Function to get a database session and close it after the request is finished
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
